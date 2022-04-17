079365aba25f43d72ebe140c3673cda8ed18c27a
Lily Yu <xiangning.yu@mail.utoronto.ca>
2022-04-16 23:41:41 -0400

    extracted classes from map_vid.txt

---
+++ extract_classes.py
@@ -0,0 +1,12 @@
+class_id_to_index = {}
+lst_classes = []
+
+with open('map_vid.txt', 'r') as f:
+    classes = f.readlines()
+    for cls in classes:
+        cls = cls.split(' ')
+        class_id_to_index[cls[0]] = int(cls[1])
+        lst_classes.append(cls[2][:-1])
+
+print(class_id_to_index)
+print(lst_classes)
